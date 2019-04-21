84468932247120688fe40d219f85643d4a2919c9
cavemancave <94838104@qq.com>
2019-04-21 19:09:41 +0800

    添加项目文件。

---
+++ stacktest/stack.cpp
@@ -0,0 +1,36 @@
+#include<stddef.h>
+#include<stdlib.h>
+#include "stdafx.h"
+#include "stack.h"
+#include <malloc.h>
+#include<windows.h>
+
+int Init(Stack &s) {
+	s.base = (Link)malloc(sizeof(Slink));
+	if (s.base == NULL) { exit(1); }
+	s.top = s.base;
+	s.len = 0;
+
+	return OK;
+}
+
+int Push(Stack &s, ElemType e) {
+	Link p = (Link)malloc(sizeof(Slink));
+	if (p == NULL) { exit(1); }
+	p->next = s.top;
+	p->data = e;
+	s.top = p;
+	s.len++;
+	return OK;
+}
+
+int Pop(Stack &s, ElemType &e) {
+	if (s.top == s.base) { return ERR; }
+
+	e = s.top->data;
+	Link p = s.top;
+	s.top = p->next;
+	free(p);
+	s.len--;
+	return OK;
+}
---
+++ stacktest/stack.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef __STACK__
+#define __STACK__
+
+#define OK 0
+#define ERR 1
+
+typedef int ElemType;
+typedef struct LinkNode {
+	struct LinkNode *next;
+	ElemType data;
+}Slink, *Link;
+
+typedef struct {
+	Link base, top;
+	int len;
+}Stack;
+
+int Init(Stack &s);
+int Push(Stack &s, ElemType e);
+int Pop(Stack &s, ElemType &e);
+
+#endif
